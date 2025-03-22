51247690a4326ea185c2c365b7cf4657e70d6fde
Vladimir Kozisck <vladimir.kozisck@gmail.com>
2025-03-22 00:02:43 -0500

    deploy cors

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const whitelist = ['http://localhost:8080', 'https://my-store-gamma-six.vercel.app'];
 const options = {
   origin: (origin, callback) => {
     if (whitelist.includes(origin)) {
